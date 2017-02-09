d4e2f2d5e5c0cbdb30502faf4cbcdeee85141e25
wzwxwc <zc_1987>
2017-02-09 16:13:59 +0800

    no comments from zcRescuer

---
+++ codes4/02dateFormat/dateTest.js
@@ -0,0 +1,44 @@
+/**
+ * Created by zcRescuer on 2017/2/9.
+ * 现在这种方式虽然可以进行呢多种的格式化，但是，我希望一种更加灵活的，支持“模板”的方式？？
+ * DateUtil中的pattern方法不错，能够通过“模板”的方式来格式化时间！
+ */
+Date.prototype.yyyymmdd = function () {
+    var yyyy = this.getFullYear();
+    var mm = this.getMonth() < 9 ? "0" + (this.getMonth() + 1) : (this.getMonth() + 1); // getMonth() is zero-based
+    var dd = this.getDate() < 10 ? "0" + this.getDate() : this.getDate();
+    return "".concat(yyyy).concat(mm).concat(dd);
+};
+
+Date.prototype.yyyymmddhh = function () {
+    var yyyy = this.getFullYear();
+    var mm = this.getMonth() < 9 ? "0" + (this.getMonth() + 1) : (this.getMonth() + 1); // getMonth() is zero-based
+    var dd = this.getDate() < 10 ? "0" + this.getDate() : this.getDate();
+    var hh = this.getHours() < 10 ? "0" + this.getHours() : this.getHours();
+    return "".concat(yyyy).concat(mm).concat(dd).concat(hh);
+};
+
+Date.prototype.yyyymmddhhmm = function () {
+    var yyyy = this.getFullYear();
+    var mm = this.getMonth() < 9 ? "0" + (this.getMonth() + 1) : (this.getMonth() + 1); // getMonth() is zero-based
+    var dd = this.getDate() < 10 ? "0" + this.getDate() : this.getDate();
+    var hh = this.getHours() < 10 ? "0" + this.getHours() : this.getHours();
+    var min = this.getMinutes() < 10 ? "0" + this.getMinutes() : this.getMinutes();
+    return "".concat(yyyy).concat(mm).concat(dd).concat(hh).concat(min);
+};
+
+Date.prototype.yyyymmddhhmmss = function () {
+    var yyyy = this.getFullYear();
+    var mm = this.getMonth() < 9 ? "0" + (this.getMonth() + 1) : (this.getMonth() + 1); // getMonth() is zero-based
+    var dd = this.getDate() < 10 ? "0" + this.getDate() : this.getDate();
+    var hh = this.getHours() < 10 ? "0" + this.getHours() : this.getHours();
+    var min = this.getMinutes() < 10 ? "0" + this.getMinutes() : this.getMinutes();
+    var ss = this.getSeconds() < 10 ? "0" + this.getSeconds() : this.getSeconds();
+    return "".concat(yyyy).concat(mm).concat(dd).concat(hh).concat(min).concat(ss);
+};
+
+var d = new Date();
+console.log(d.yyyymmdd());
+console.log(d.yyyymmddhh());
+console.log(d.yyyymmddhhmm());
+console.log(d.yyyymmddhhmmss());
