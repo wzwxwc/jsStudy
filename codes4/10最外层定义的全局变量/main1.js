bb4863524cf99ccf28e2d7a08d7f1b070ac42451
wzwxwc <zc_1987>
2017-07-13 09:10:02 +0800

    no comments from zcRescuer

---
+++ codes4/10最外层定义的全局变量/main1.js
@@ -0,0 +1,9 @@
+/**
+ * Created by zc on 2017/7/13.
+ */
+var global_a = "aaa";
+function test() {
+    var a = 123;
+    console.log(a);
+}
+test();
---
+++ codes4/10最外层定义的全局变量/main2.js
@@ -0,0 +1,8 @@
+/**
+ * Created by zc on 2017/7/13.
+ */
+var global_b = "bbb";
+function bb() {
+    console.log(global_a);
+}
+bb();
